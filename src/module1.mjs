a6ecb381ad3b94fcb3945f9288d101f3f6e2c380
Aman Shukla <aman.sh22.11@gmail.com>
2023-02-20 16:32:45 +0530

    initial commit in textUtil folder

---
+++ src/module1.mjs
@@ -0,0 +1,7 @@
+const a = "Aman";
+const b = "ayushi";
+const c = "rohan";
+const d = "rohit";
+
+export default a;
+export {b, c, d}
---
+++ src/module2.mjs
@@ -0,0 +1,4 @@
+import name from "./module1.mjs"
+import {b, c, d} from "./module1.mjs"
+console.log(name);
+console.log(b, c, d);
